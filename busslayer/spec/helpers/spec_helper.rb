da0abde921090fe972ee15ad58365f02031aa2e7
azranel <chunkyo@gmail.com>
2014-10-27 21:36:44 +0100

    added tests for users API and cleaning db after each test suite run

--- busslayer/spec/helpers/spec_helper.rb
+++ busslayer/spec/helpers/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rack/test'
+require 'database_cleaner'
 
-#require File.expand_path '../../../app.rb', __FILE__
 require_relative File.join('../..', 'app')
 
 ENV['RACK_ENV'] = 'test'
@@ -11,4 +11,12 @@ module RSpecMixin
 end
 
 # For RSpec 2.x
-RSpec.configure { |c| c.include RSpecMixin }
+RSpec.configure do |config|
+  config.include RSpecMixin
+  config.color = true
+  config.tty = true
+  config.after(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  ActiveRecord::Base.logger.level = 1
+end
