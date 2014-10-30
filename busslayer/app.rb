66fe0f795be35b1cfe5645b7fd5137cf684df14e
azranel <chunkyo@gmail.com>
2014-10-30 13:27:21 +0100

    Added dRb (kind of RPC) function

--- busslayer/app.rb
+++ busslayer/app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require "sinatra/activerecord"
 require "i18n"
 require "json"
+require 'drb'
+require 'thread'
 
 #Used for loading models into the application
 Dir["./models/*.rb"].each { |file| require file }
@@ -11,15 +13,15 @@ I18n.config.enforce_available_locales = false
 #Used for loading routes
 Dir["./routes/*.rb"].each { |file| require file }
 
+require "./rmiserv"
 ENV['RACK_ENV'] ||= 'development'
+RMI_URL = 'druby://0.0.0.0:9000'
 
 
 class SimpleApp < Sinatra::Base
  
   set :root, File.dirname(__FILE__)
   set :database_file, "./db/database.yml"
-
-  
 
   enable :sessions
  
@@ -28,5 +30,13 @@ class SimpleApp < Sinatra::Base
   register Sinatra::Routing::Apartments
   register Sinatra::Routing::Misc
 
+  thr = Thread.new { DRb.start_service(RMI_URL, RMIServer.new) }
+  
+  #DRb.thread.join
+  puts "RMIServer running at #{RMI_URL}"
+
   run! if app_file == $0
+
+  # replace localhost with 0.0.0.0 to allow conns from outside
+  
 end
---
+++ busslayer/rmiserv.rb
@@ -0,0 +1,18 @@
+# Remote Method Invocation Server
+class RMIServer
+  def initialize
+  end
+
+  def add_user(params)
+    User.create(params)
+  end
+
+  def fetch_all_users
+    User.all.as_json
+  end
+
+  def add_apartment(params)
+    Apartment.create(params)
+  end
+end
+
